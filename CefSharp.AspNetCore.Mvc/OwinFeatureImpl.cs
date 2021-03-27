1f22e138d57dc69c105623796ac7c3bbb21bbb73
Alex Maitland <amaitland@users.noreply.github.com>
2021-03-27 14:51:09 +1000

    Code cleanup

--- CefSharp.AspNetCore.Mvc/OwinFeatureImpl.cs
+++ CefSharp.AspNetCore.Mvc/OwinFeatureImpl.cs
@@ -42,7 +42,7 @@ namespace CefSharp.AspNetCore.Mvc
             Environment = environment;
         }
 
-        T Prop<T>(string key)
+        private T GetEnvironmentPropertyOrDefault<T>(string key)
         {
             object value;
             if (Environment.TryGetValue(key, out value) && value is T)
@@ -52,45 +52,45 @@ namespace CefSharp.AspNetCore.Mvc
             return default(T);
         }
 
-        void Prop(string key, object value)
+        private void SetEnvironmentProperty(string key, object value)
         {
             Environment[key] = value;
         }
 
         string IHttpRequestFeature.Protocol
         {
-            get { return Prop<string>(OwinConstants.RequestProtocol); }
-            set { Prop(OwinConstants.RequestProtocol, value); }
+            get { return GetEnvironmentPropertyOrDefault<string>(OwinConstants.RequestProtocol); }
+            set { SetEnvironmentProperty(OwinConstants.RequestProtocol, value); }
         }
 
         string IHttpRequestFeature.Scheme
         {
-            get { return Prop<string>(OwinConstants.RequestScheme); }
-            set { Prop(OwinConstants.RequestScheme, value); }
+            get { return GetEnvironmentPropertyOrDefault<string>(OwinConstants.RequestScheme); }
+            set { SetEnvironmentProperty(OwinConstants.RequestScheme, value); }
         }
 
         string IHttpRequestFeature.Method
         {
-            get { return Prop<string>(OwinConstants.RequestMethod); }
-            set { Prop(OwinConstants.RequestMethod, value); }
+            get { return GetEnvironmentPropertyOrDefault<string>(OwinConstants.RequestMethod); }
+            set { SetEnvironmentProperty(OwinConstants.RequestMethod, value); }
         }
 
         string IHttpRequestFeature.PathBase
         {
-            get { return Prop<string>(OwinConstants.RequestPathBase); }
-            set { Prop(OwinConstants.RequestPathBase, value); }
+            get { return GetEnvironmentPropertyOrDefault<string>(OwinConstants.RequestPathBase); }
+            set { SetEnvironmentProperty(OwinConstants.RequestPathBase, value); }
         }
 
         string IHttpRequestFeature.Path
         {
-            get { return Prop<string>(OwinConstants.RequestPath); }
-            set { Prop(OwinConstants.RequestPath, value); }
+            get { return GetEnvironmentPropertyOrDefault<string>(OwinConstants.RequestPath); }
+            set { SetEnvironmentProperty(OwinConstants.RequestPath, value); }
         }
 
         string IHttpRequestFeature.QueryString
         {
-            get { return AddQuestionMark(Prop<string>(OwinConstants.RequestQueryString)); }
-            set { Prop(OwinConstants.RequestQueryString, RemoveQuestionMark(value)); }
+            get { return AddQuestionMark(GetEnvironmentPropertyOrDefault<string>(OwinConstants.RequestQueryString)); }
+            set { SetEnvironmentProperty(OwinConstants.RequestQueryString, RemoveQuestionMark(value)); }
         }
 
         string IHttpRequestFeature.RawTarget
@@ -105,36 +105,36 @@ namespace CefSharp.AspNetCore.Mvc
             {
                 if(_requestHeaders == null)
                 {
-                    var dict = Prop<IDictionary<string, string[]>>(OwinConstants.RequestHeaders);
+                    var dict = GetEnvironmentPropertyOrDefault<IDictionary<string, string[]>>(OwinConstants.RequestHeaders);
                     _requestHeaders = dict is IHeaderDictionary ? (IHeaderDictionary)dict : new DictionaryStringValuesWrapper(dict);
                 }
                 return _requestHeaders;
             }
-            set { Prop(OwinConstants.RequestHeaders, MakeDictionaryStringArray(value)); }
+            set { SetEnvironmentProperty(OwinConstants.RequestHeaders, MakeDictionaryStringArray(value)); }
         }
 
         string IHttpRequestIdentifierFeature.TraceIdentifier
         {
-            get { return Prop<string>(OwinConstants.RequestId); }
-            set { Prop(OwinConstants.RequestId, value); }
+            get { return GetEnvironmentPropertyOrDefault<string>(OwinConstants.RequestId); }
+            set { SetEnvironmentProperty(OwinConstants.RequestId, value); }
         }
 
         Stream IHttpRequestFeature.Body
         {
-            get { return Prop<Stream>(OwinConstants.RequestBody); }
-            set { Prop(OwinConstants.RequestBody, value); }
+            get { return GetEnvironmentPropertyOrDefault<Stream>(OwinConstants.RequestBody); }
+            set { SetEnvironmentProperty(OwinConstants.RequestBody, value); }
         }
 
         int IHttpResponseFeature.StatusCode
         {
-            get { return Prop<int>(OwinConstants.ResponseStatusCode); }
-            set { Prop(OwinConstants.ResponseStatusCode, value); }
+            get { return GetEnvironmentPropertyOrDefault<int>(OwinConstants.ResponseStatusCode); }
+            set { SetEnvironmentProperty(OwinConstants.ResponseStatusCode, value); }
         }
 
         string IHttpResponseFeature.ReasonPhrase
         {
-            get { return Prop<string>(OwinConstants.ResponseReasonPhrase); }
-            set { Prop(OwinConstants.ResponseReasonPhrase, value); }
+            get { return GetEnvironmentPropertyOrDefault<string>(OwinConstants.ResponseReasonPhrase); }
+            set { SetEnvironmentProperty(OwinConstants.ResponseReasonPhrase, value); }
         }
 
         IHeaderDictionary IHttpResponseFeature.Headers
@@ -143,23 +143,23 @@ namespace CefSharp.AspNetCore.Mvc
             {
                 if (_responseHeaders == null)
                 {
-                    var dict = Prop<IDictionary<string, string[]>>(OwinConstants.ResponseHeaders);
+                    var dict = GetEnvironmentPropertyOrDefault<IDictionary<string, string[]>>(OwinConstants.ResponseHeaders);
                     _responseHeaders = dict is IHeaderDictionary ? (IHeaderDictionary)dict : new DictionaryStringValuesWrapper(dict);
                 }
                 return _responseHeaders;
             }
-            set { Prop(OwinConstants.ResponseHeaders, MakeDictionaryStringArray(value)); }
+            set { SetEnvironmentProperty(OwinConstants.ResponseHeaders, MakeDictionaryStringArray(value)); }
         }
 
         Stream IHttpResponseFeature.Body
         {
-            get { return Prop<Stream>(OwinConstants.ResponseBody); }
-            set { Prop(OwinConstants.ResponseBody, value); }
+            get { return GetEnvironmentPropertyOrDefault<Stream>(OwinConstants.ResponseBody); }
+            set { SetEnvironmentProperty(OwinConstants.ResponseBody, value); }
         }
 
         Stream IHttpResponseBodyFeature.Stream
         {
-            get { return Prop<Stream>(OwinConstants.ResponseBody); }
+            get { return GetEnvironmentPropertyOrDefault<Stream>(OwinConstants.ResponseBody); }
         }
 
         PipeWriter IHttpResponseBodyFeature.Writer
@@ -168,7 +168,7 @@ namespace CefSharp.AspNetCore.Mvc
             {
                 if (_responseBodyWrapper == null)
                 {
-                    _responseBodyWrapper = PipeWriter.Create(Prop<Stream>(OwinConstants.ResponseBody), new StreamPipeWriterOptions(leaveOpen: true));
+                    _responseBodyWrapper = PipeWriter.Create(GetEnvironmentPropertyOrDefault<Stream>(OwinConstants.ResponseBody), new StreamPipeWriterOptions(leaveOpen: true));
                 }
 
                 return _responseBodyWrapper;
@@ -209,7 +209,7 @@ namespace CefSharp.AspNetCore.Mvc
             }
 
             // The pipe may or may not have flushed the stream. Make sure the stream gets flushed to trigger response start.
-            await Prop<Stream>(OwinConstants.ResponseBody).FlushAsync(cancellationToken);
+            await GetEnvironmentPropertyOrDefault<Stream>(OwinConstants.ResponseBody).FlushAsync(cancellationToken);
         }
 
         Task IHttpResponseBodyFeature.CompleteAsync()
