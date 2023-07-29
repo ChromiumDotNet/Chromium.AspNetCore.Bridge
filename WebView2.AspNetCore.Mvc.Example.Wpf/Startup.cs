be4ab9d27ff20aeab1d1a78d4334e46255839624
marman_hp <marman.hadi.pranoto@gmail.com>
2023-07-29 21:22:36 +0700

    Added refrence

--- WebView2.AspNetCore.Mvc.Example.Wpf/Startup.cs
+++ WebView2.AspNetCore.Mvc.Example.Wpf/Startup.cs
@@ -1,8 +1,12 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System;
+using System.Threading.Tasks;
+
 
 namespace WebView2.AspNetCore.Mvc.Example.Wpf
 {
